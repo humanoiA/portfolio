bc89caa917c7d67bc14db2251c9ef8083d33282a
Shubhang Sharma <shubhang.sharma123@live.com>
2020-08-31 18:57:58 +0530

    Updates

---
+++ precache-manifest.ed412cf23e8ee94f9395ced21cb09bc4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb91864e63a76d70d40b",
+    "url": "/portfolio/static/css/main.a41096be.chunk.css"
+  },
+  {
+    "revision": "bb91864e63a76d70d40b",
+    "url": "/portfolio/static/js/main.bb91864e.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "aeb4cdaa3b2c79bfca1f4f30be7e9e66",
+    "url": "/portfolio/index.html"
+  }
+];
